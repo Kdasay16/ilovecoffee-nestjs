67199dd067bed638cef8841901d8c0bd25299ab1
Kdasay16 <kayla@kayla-asay.com>
2022-01-31 18:54:47 -0800

    completed chapter 3

--- src/coffees/entities/coffee.entity.ts
+++ src/coffees/entities/coffee.entity.ts
@@ -1,6 +1,39 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  JoinTable,
+  ManyToMany
+} from 'typeorm';
+import { Flavor } from './flavor.entity';
+// entity represents the relationship between a typescript class and a db table
+// you can type the db name in the entity, but classname Coffee will make a coffee table
+
+@Entity()
 export class Coffee {
+  @PrimaryGeneratedColumn()
   id: number;
+
+  @Column()
   name: string;
+
+  @Column({ nullable: true })
+  description: string;
+
+  @Column()
   brand: string;
-  flavors: string[];
+
+  @Column({ default: 0 })
+  recommendations: number;
+
+  // relations are not egerlly loaded by default, so you must specify relations in coffees.service
+  @JoinTable() // this is only needed in the ower of the property
+  @ManyToMany(
+    type => Flavor,
+    (flavor) => flavor.coffees,
+    {
+      cascade: true // ['insert new flavors automatically']
+    }
+  )
+  flavors: Flavor[];
 }
--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,10 @@ async function bootstrap() {
     new ValidationPipe({
       whitelist: true, // this removes unwanted or invalid properties
       transform: true, // transforms objects to match the dto object
-      forbidNonWhitelisted: true // when true, this will return an error if a whitelisted item is sent
+      forbidNonWhitelisted: true, // when true, this will return an error if a whitelisted item is sent
+      transformOptions: {
+        enableImplicitConversion: true
+      }
     })
   );
   await app.listen(3000);
